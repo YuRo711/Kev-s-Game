0b8a066cc9b1decc9e32b78be5de5ff8ca1eec5f
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-06-04 19:59:15 +0300

    vertical velocity

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -9,19 +9,32 @@ namespace Player
 
         [SerializeField] private CharacterController characterController;
         [SerializeField] private float speed = 10f;
+        [SerializeField] private float gravity = -9.8f;
+
+        private bool _isGrounded;
+        private float _ySpeed;
 
         #endregion
 
         #region Private Methods
+
+        private void ApplyGravity()
+        {
+            _isGrounded = characterController.isGrounded;
+            if (_isGrounded)
+                _ySpeed = 0;
+            else
+                _ySpeed += gravity * Time.deltaTime;
+        }
 
         private void Move()
         {
             var deltaX = Input.GetAxis("Horizontal");
             var deltaZ = Input.GetAxis("Vertical");
 
-            var moveVector = transform.right * deltaX + transform.forward * deltaZ;
-
-            characterController.Move(moveVector * (Time.deltaTime * speed));
+            characterController.Move(
+                transform.TransformDirection(new Vector3(deltaX, _ySpeed , deltaZ)) 
+                * (Time.deltaTime * speed));
         }
 
         #endregion
@@ -30,6 +43,7 @@ namespace Player
 
         private void Update()
         {
+            _isGrounded = characterController.isGrounded;
             Move();
         }
 
