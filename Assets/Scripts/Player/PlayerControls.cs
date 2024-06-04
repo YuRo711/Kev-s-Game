7d74a87d66156a7563f058d925a8acac118fd50f
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-06-04 19:28:27 +0300

    horizontal camera rotation

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerControls : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform cameraTransform;
+
+    private float _rotationX;
+
+    #endregion
+
+    #region Private Methods
+
+    private void ProcessMouseMovement()
+    {
+        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _rotationX = Math.Clamp(_rotationX - mouseY, -45f, 45f);
+        
+        cameraTransform.Rotate(Vector3.up * mouseX);
+    }
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        ProcessMouseMovement();
+    }
+
+    #endregion
+}
